3b8fd3a9c1ae2390969850b3266ef9072f4abd8d
Adityaa11-code <winstrek212@gmail.com>
2025-12-15 14:46:14 +0700

    “Commit No.5 – add back button & extend list”

--- app/src/main/java/com/example/lab_week_03/DetailFragment.kt
+++ app/src/main/java/com/example/lab_week_03/DetailFragment.kt
@@ -4,16 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 
 class DetailFragment : Fragment() {
-
-    private val coffeeTitle: TextView?
-        get() = view?.findViewById(R.id.coffee_title)
-
-    private val coffeeDesc: TextView?
-        get() = view?.findViewById(R.id.coffee_desc)
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,23 +21,34 @@ class DetailFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val coffeeId = arguments?.getInt(ListFragment.COFFEE_ID, 0) ?: 0
-        setCoffeeData(coffeeId)
-    }
+        val title = view.findViewById<TextView>(R.id.coffee_title)
+        val desc = view.findViewById<TextView>(R.id.coffee_desc)
+        val backBtn = view.findViewById<Button>(R.id.btn_back)
 
-    private fun setCoffeeData(id: Int) {
-        when (id) {
+        backBtn.setOnClickListener {
+            it.findNavController().navigateUp()
+        }
+
+        when (arguments?.getInt(ListFragment.COFFEE_ID)) {
             R.id.affogato -> {
-                coffeeTitle?.text = getString(R.string.affogato_title)
-                coffeeDesc?.text = getString(R.string.affogato_desc)
+                title.text = getString(R.string.affogato_title)
+                desc.text = getString(R.string.affogato_desc)
             }
             R.id.americano -> {
-                coffeeTitle?.text = getString(R.string.americano_title)
-                coffeeDesc?.text = getString(R.string.americano_desc)
+                title.text = getString(R.string.americano_title)
+                desc.text = getString(R.string.americano_desc)
             }
             R.id.latte -> {
-                coffeeTitle?.text = getString(R.string.latte_title)
-                coffeeDesc?.text = getString(R.string.latte_desc)
+                title.text = getString(R.string.latte_title)
+                desc.text = getString(R.string.latte_desc)
+            }
+            R.id.cappuccino -> {
+                title.text = getString(R.string.cappuccino_title)
+                desc.text = getString(R.string.cappuccino_desc)
+            }
+            R.id.mocha -> {
+                title.text = getString(R.string.mocha_title)
+                desc.text = getString(R.string.mocha_desc)
             }
         }
     }
